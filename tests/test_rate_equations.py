e6fad25e58bf1e7988bb250868a2da2926c99aaf
teddygroves <groves.teddy@gmail.com>
2024-08-20 17:35:08 +0200

    update tests

--- tests/test_rate_equations.py
+++ tests/test_rate_equations.py
@@ -1,6 +1,5 @@
 """Unit tests for rate equations."""
 
-import pytest
 from jax import numpy as jnp
 from enzax.rate_equations import (
     AllostericIrreversibleMichaelisMenten,
@@ -8,22 +7,110 @@ from enzax.rate_equations import (
     IrreversibleMichaelisMenten,
     ReversibleMichaelisMenten,
 )
-from enzax.examples import linear
+from enzax.kinetic_model import KineticModelParameters
 
-
-@pytest.mark.parametrize(
-    ["rate_equation", "expected_rate"],
-    [
-        (IrreversibleMichaelisMenten, 0.01105354),
-        (ReversibleMichaelisMenten, -0.01188049),
-        (AllostericIrreversibleMichaelisMenten, 0.00474257),
-        (AllostericReversibleMichaelisMenten, -0.00535258),
-    ],
+EXAMPLE_CONC = jnp.array([0.5, 0.2, 0.1, 0.3])
+EXAMPLE_PARAMETERS = KineticModelParameters(
+    log_kcat=jnp.array([-0.1]),
+    log_enzyme=jnp.log(jnp.array([0.3])),
+    dgf=jnp.array([-3, -1.0]),
+    log_km=jnp.array([0.1, -0.2]),
+    log_ki=jnp.array([1.0]),
+    log_conc_unbalanced=jnp.log(jnp.array([0.5, 0.3])),
+    temperature=jnp.array(310.0),
+    log_transfer_constant=jnp.array([-0.2, 0.3]),
+    log_dissociation_constant=jnp.array([-0.1, 0.2]),
+    log_drain=jnp.array([]),
 )
-def test_rate_equations_linear_one(rate_equation, expected_rate):
-    """Check output of different rate equations for rate 1 of linear model."""
-    conc = jnp.array([0.5, 0.2, 0.1, 0.3])
-    ix = 1
-    f = rate_equation(linear.parameters, linear.structure, ix)
-    rate = f(conc[linear.structure.rate_to_reactants[ix]])
+
+
+def test_irreversible_michaelis_menten():
+    expected_rate = 0.08455524
+    f = IrreversibleMichaelisMenten(
+        kcat_ix=0,
+        enzyme_ix=0,
+        km_ix=jnp.array([0, 1], dtype=jnp.int16),
+        ki_ix=jnp.array([], dtype=jnp.int16),
+        reactant_stoichiometry=jnp.array([-1, 1], dtype=jnp.int16),
+        ix_ki_species=jnp.array([], dtype=jnp.int16),
+        substrate_km_positions=jnp.array([0], dtype=jnp.int16),
+        substrate_reactant_positions=jnp.array([0], dtype=jnp.int16),
+        ix_substrate=jnp.array([0], dtype=jnp.int16),
+    )
+    rate = f(EXAMPLE_CONC, EXAMPLE_PARAMETERS)
+    assert jnp.isclose(rate, expected_rate)
+
+
+def test_reversible_michaelis_menten():
+    expected_rate = 0.04342889
+    f = ReversibleMichaelisMenten(
+        kcat_ix=0,
+        enzyme_ix=0,
+        km_ix=jnp.array([0, 1], dtype=jnp.int16),
+        ki_ix=jnp.array([], dtype=jnp.int16),
+        reactant_stoichiometry=jnp.array([-1, 1], dtype=jnp.int16),
+        reactant_to_dgf=jnp.array([0, 0], dtype=jnp.int16),
+        ix_ki_species=jnp.array([], dtype=jnp.int16),
+        substrate_km_positions=jnp.array([0], dtype=jnp.int16),
+        substrate_reactant_positions=jnp.array([0], dtype=jnp.int16),
+        ix_substrate=jnp.array([0], dtype=jnp.int16),
+        ix_product=jnp.array([1], dtype=jnp.int16),
+        ix_reactants=jnp.array([0, 1], dtype=jnp.int16),
+        product_reactant_positions=jnp.array([1], dtype=jnp.int16),
+        product_km_positions=jnp.array([1], dtype=jnp.int16),
+        water_stoichiometry=jnp.array(0.0),
+    )
+    rate = f(EXAMPLE_CONC, EXAMPLE_PARAMETERS)
+    assert jnp.isclose(rate, expected_rate)
+
+
+def test_allosteric_irreversible_michaelis_menten():
+    expected_rate = 0.05608589
+    f = AllostericIrreversibleMichaelisMenten(
+        kcat_ix=0,
+        enzyme_ix=0,
+        km_ix=jnp.array([0, 1], dtype=jnp.int16),
+        ki_ix=jnp.array([], dtype=jnp.int16),
+        reactant_stoichiometry=jnp.array([-1, 1], dtype=jnp.int16),
+        ix_ki_species=jnp.array([], dtype=jnp.int16),
+        substrate_km_positions=jnp.array([0], dtype=jnp.int16),
+        substrate_reactant_positions=jnp.array([0], dtype=jnp.int16),
+        ix_substrate=jnp.array([0], dtype=jnp.int16),
+        tc_ix=0,
+        ix_dc_inhibition=jnp.array([], dtype=jnp.int16),
+        ix_dc_activation=jnp.array([0], dtype=jnp.int16),
+        species_activation=jnp.array([2], dtype=jnp.int16),
+        species_inhibition=jnp.array([], dtype=jnp.int16),
+        subunits=1,
+    )
+    rate = f(EXAMPLE_CONC, EXAMPLE_PARAMETERS)
+    assert jnp.isclose(rate, expected_rate)
+
+
+def test_allosteric_reversible_michaelis_menten():
+    expected_rate = 0.03027414
+    f = AllostericReversibleMichaelisMenten(
+        kcat_ix=0,
+        enzyme_ix=0,
+        km_ix=jnp.array([0, 1], dtype=jnp.int16),
+        ki_ix=jnp.array([], dtype=jnp.int16),
+        reactant_stoichiometry=jnp.array([-1, 1], dtype=jnp.int16),
+        reactant_to_dgf=jnp.array([0, 0], dtype=jnp.int16),
+        ix_ki_species=jnp.array([], dtype=jnp.int16),
+        substrate_km_positions=jnp.array([0], dtype=jnp.int16),
+        substrate_reactant_positions=jnp.array([0], dtype=jnp.int16),
+        ix_substrate=jnp.array([0], dtype=jnp.int16),
+        ix_product=jnp.array([1], dtype=jnp.int16),
+        ix_reactants=jnp.array([0, 1], dtype=jnp.int16),
+        product_reactant_positions=jnp.array([1], dtype=jnp.int16),
+        product_km_positions=jnp.array([1], dtype=jnp.int16),
+        water_stoichiometry=jnp.array(0.0),
+        tc_ix=0,
+        ix_dc_inhibition=jnp.array([], dtype=jnp.int16),
+        ix_dc_activation=jnp.array([0], dtype=jnp.int16),
+        species_activation=jnp.array([2], dtype=jnp.int16),
+        species_inhibition=jnp.array([], dtype=jnp.int16),
+        subunits=1,
+    )
+    rate = f(EXAMPLE_CONC, EXAMPLE_PARAMETERS)
     assert jnp.isclose(rate, expected_rate)
