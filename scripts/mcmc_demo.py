bug: correcting covariance matrix

scripts/mcmc_demo.py
@@ -40,7 +40,7 @@ def main():
         log_drain=ind_prior_from_truth(true_parameters.log_drain, 0.1),
         dgf=(
             ind_prior_from_truth(true_parameters.dgf, 0.1)[0],
-            jnp.diag(ind_prior_from_truth(true_parameters.dgf, 0.1)[1]),
+            jnp.diag(jnp.square(ind_prior_from_truth(true_parameters.dgf, 0.1)[1])),
         ),
         log_km=ind_prior_from_truth(true_parameters.log_km, 0.1),
         log_conc_unbalanced=ind_prior_from_truth(