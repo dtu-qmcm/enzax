95ccc42ee1ed53c0bff21310fdde0ffeb5038a29
AlberteSloth <158040192+AlberteSloth@users.noreply.github.com>
2024-11-15 11:05:30 +0100

    sbml module and sbml test

---
+++ src/enzax/sbml.py
@@ -0,0 +1,22 @@
+import libsbml
+from sbmlmath import SBMLMathMLPrinter, SBMLMathMLParser
+
+def load_sbml(file_path):
+    reader = libsbml.SBMLReader()
+    doc = reader.readSBML(file_path)
+    if doc.getModel().getNumFunctionDefinitions():
+        convert_config = libsbml.SBMLFunctionDefinitionConverter().getDefaultProperties()
+        doc.convert(convert_config)
+    model = doc.getModel()
+    if model is None:
+        raise ValueError("Failed to load the SBML model")
+    return model
+
+def sbml_to_sympy(model):
+    reactions_sbml = model.getListOfReactions()
+    reactions_sympy = [(SBMLMathMLParser().parse_str(
+        libsbml.writeMathMLToString(
+            libsbml.parseL3Formula(
+                libsbml.formulaToL3String(r.getKineticLaw().getMath()))))
+                ) for r in reactions_sbml]
+    return reactions_sympy
