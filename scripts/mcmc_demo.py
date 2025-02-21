440c623519970c38a7a3c31d0dd65fe5a49436a8
Teddy Groves <groves.teddy@gmail.com>
2025-02-21 16:53:46 +0100

    Methionine demo works up to creating idata

--- scripts/mcmc_demo.py
+++ scripts/mcmc_demo.py
@@ -15,13 +15,24 @@ from enzax.examples import methionine
 from enzax.kinetic_model import get_conc
 from enzax.mcmc import get_idata, run_nuts
 from enzax.steady_state import get_kinetic_model_steady_state
-from enzax.statistical_modelling import enzax_log_density
+from enzax.statistical_modelling import enzax_log_density, prior_from_truth
 
 import equinox as eqx
 
 SEED = 1234
 
 jax.config.update("jax_enable_x64", True)
+
+
+def simulate(key, truth, error):
+    key_conc, key_enz, key_flux = jax.random.split(key, num=3)
+    true_conc, true_log_enz, true_flux = truth
+    conc_err, enz_err, flux_err = error
+    return (
+        jnp.exp(jnp.log(true_conc) + jax.random.normal(key_conc) * conc_err),
+        jnp.exp(true_log_enz + jax.random.normal(key_enz) * enz_err),
+        true_flux + jax.random.normal(key_flux) * flux_err,
+    )
 
 
 def main():
@@ -38,51 +49,45 @@ def main():
             params["dgf"],
         )
 
-    false_tree = jax.tree.map(lambda _: False, true_parameters)
-    freespec = eqx.tree_at(
+    is_free = eqx.tree_at(
         get_free_params,
-        false_tree,
+        jax.tree.map(lambda _: False, true_parameters),
         replace_fn=lambda _: True,
     )
-    free_params, fixed_params = eqx.partition(true_parameters, freespec)
-    prior_mean = free_params
-    prior_sd = jax.tree.map(lambda arr: jnp.full_like(arr, 0.1), prior_mean)
-    prior = jax.tree.transpose(
-        outer_treedef=jax.tree.structure(("*", "*")),
-        inner_treedef=jax.tree.structure(prior_mean),
-        pytree_to_transpose=[prior_mean, prior_sd],
+    free_params, fixed_params = eqx.partition(true_parameters, is_free)
+    is_mv = eqx.tree_at(
+        lambda params: params["dgf"],
+        jax.tree.map(lambda _: False, free_params),
+        replace=True,
     )
-    # get true concentration
+    prior = prior_from_truth(free_params, sd=0.1, is_multivariate=is_mv)
+    # get true concentration, flux and log enzyme
     true_conc = get_conc(
         true_steady,
         true_parameters["log_conc_unbalanced"],
         methionine.structure,
     )
-    # get true flux
     true_flux = true_model.flux(true_steady)
-    # simulate observations
-    error_conc = 0.03
-    error_flux = 0.05
-    error_enzyme = 0.03
-    key = jax.random.key(SEED)
     true_log_enz_flat, _ = ravel_pytree(true_parameters["log_enzyme"])
-    key_conc, key_enz, key_flux, key_nuts = jax.random.split(key, num=4)
-    obs_conc = jnp.exp(
-        jnp.log(true_conc) + jax.random.normal(key_conc) * error_conc
+    # simulate observations
+    conc_err = jnp.full_like(true_conc, 0.03)
+    flux_err = jnp.full_like(true_flux, 0.05)
+    enz_err = jnp.full_like(true_log_enz_flat, 0.03)
+    key = jax.random.key(SEED)
+    key_sim, key_nuts = jax.random.split(key, num=2)
+    measurement_errors = (conc_err, enz_err, flux_err)
+    measurement_values = simulate(
+        key=key_sim,
+        truth=(true_conc, true_log_enz_flat, true_flux),
+        error=measurement_errors,
     )
-    obs_enzyme = jnp.exp(
-        true_log_enz_flat + jax.random.normal(key_enz) * error_enzyme
-    )
-    obs_flux = true_flux + jax.random.normal(key_flux) * error_flux
-    print(obs_conc)
-    print(obs_enzyme)
-    print(obs_flux)
+    measurements = tuple(zip(measurement_values, measurement_errors))
     posterior_log_density = jax.jit(
         functools.partial(
             enzax_log_density,
             structure=true_model.structure,
             fixed_parameters=fixed_params,
-            observations=[],
+            measurements=measurements,
             prior=prior,
             guess=default_guess,
         )
--- src/enzax/statistical_modelling.py
+++ src/enzax/statistical_modelling.py
@@ -1,6 +1,3 @@
-from copy import deepcopy
-from dataclasses import dataclass
-from functools import partial
 import operator
 from typing import Union
 
@@ -11,60 +8,14 @@ from enzax.kinetic_model import (
 )
 from enzax.steady_state import get_kinetic_model_steady_state
 import jax
-from jax.flatten_util import ravel_pytree
 from jax import numpy as jnp
+from jax.flatten_util import ravel_pytree
 from jax.scipy.stats import multivariate_normal, norm
 from jaxtyping import Array, Float, PyTree, Scalar
 import equinox as eqx
 
 FloatArray = Float[Array, " _"]
 ParamDict = dict[str, Union[FloatArray, "ParamDict"]]
-
-
-@dataclass
-class Measurement:
-    id: str
-    value: float
-    error_sd: float
-
-
-@dataclass
-class ObservationSet:
-    """Measurements from a single experiment."""
-
-    id: str
-    conc: list[Measurement]
-    flux: list[Measurement]
-    enzyme: list[Measurement]
-
-
-@dataclass
-class FreeParamSpec:
-    path: tuple[str, ...]
-    ix: tuple[tuple[int, ...], ...]
-    inits: Float[Array, " _"]
-
-
-@dataclass
-class FixedParamSpec:
-    path: tuple[str, ...]
-    ix: tuple[tuple[int, ...], ...]
-    fixed_values: Float[Array, " _"]
-
-
-def ind_normal_prior_logdensity(param, prior: Float[Array, "2 _"]):
-    """Total log density for an independent normal distribution."""
-    return norm.logpdf(param, loc=prior[0], scale=prior[1]).sum()
-
-
-def mv_normal_prior_logdensity(
-    param: Float[Array, " _"],
-    prior: tuple[Float[Array, " _"], Float[Array, " _ _"]],
-):
-    """Total log density for an multivariate normal distribution."""
-    return jnp.sum(
-        multivariate_normal.logpdf(param, mean=prior[0], cov=prior[1])
-    )
 
 
 def prior_from_truth(
@@ -74,88 +25,152 @@ def prior_from_truth(
 ) -> PyTree:
     """Get a PyTree of priors from a PyTree of true values and an sd.
 
-    The return value is a pytree of tuples, where the first value is the prior
-     location (the same as true_params) and the second value is the prior
-    scale (a 1D array with the same length as true_params, filled with sd).
+    Params:
+    -------
+    true_params: PyTree
+        A PyTree of true parameter values.
+    sd: float
+        The standard deviation of the prior.
+    is_multivariate: PyTree[bool] | None
+        An optional PyTree of booleans indicating whether each parameter is
+        multivariate. If None, all parameters are assumed to be univariate.
 
-    If is_multivariate is provided, then the prior scale for true leaves is a
-    square matrix with sd along the diagonal.
+    Returns:
+    --------
+    PyTree
+        A PyTree of priors, where each leaf is a tuple of the prior location
+        and the prior scale (i.e. the argument sd repeated as appropriate).
 
     """
 
-    def prior_leaf_from_truth(
-        true_leaf: Float[Array, " k"],
-        is_multivariate=False,
-    ) -> tuple[Float[Array, " k"], Float[Array, "..."]]:
-        sd_arr = jnp.full((len(true_leaf),), sd)
-        if is_multivariate:
+    def get_scale_for_leaf(true_leaf: jax.Array, is_mv: bool) -> jax.Array:
+        sd_arr = jnp.full_like(true_leaf, sd)
+        if is_mv:
             sd_arr = jnp.diag(sd_arr)
-        return (true_leaf, sd_arr)
+        return sd_arr
 
     if is_multivariate is None:
         is_multivariate = jax.tree.map(lambda leaf: False, true_params)
-    return jax.tree.map(prior_leaf_from_truth, true_params, is_multivariate)
+    scale = jax.tree.map(get_scale_for_leaf, true_params, is_multivariate)
+    return pack_locs_and_scales(loc=true_params, scale=scale)
 
 
-def merge_fixed_and_free_parameters(free: PyTree, fixed: PyTree) -> PyTree:
-    """Combine a Pytree of fixed parameters with a Pytree of free parameters.
+def pack_locs_and_scales(loc: PyTree, scale: PyTree) -> PyTree:
+    """Get a Pytree whose leaves are tuples of loc and scale.
 
-    Inputs fixed and free must have the same structure, and it is assumed that
-    one of the following three cases holds for each leaf:
+     Note that loc and scale must have the same tree structure.
 
-    Case 1: The leaf of fixed is None. In this case the leaf of free is output.
+     Params:
+     -------
+     loc: PyTree
+         A PyTree representing location parameters.
+     scale: PyTree
+         A PyTree representing scale parameters.
 
-    Case 2: The leaf of free is None. In this case the leaf of fixed is output.
+     Returns:
+     --------
+     PyTree
+         A PyTree whose leaves are tuples of loc and scale.
 
-    Case 3: Both leaves are JAX numpy arrays with dtype float. In this case the
-    free array should be non-null, with exactly as many members as there are
-    null values in the fixed array. The output is the result of replacing the
-    null values of the fixed leaf with the values of the free leaf.
+     Examples:
+     ---------
+     >>> loc = {"a": jnp.array([1.0, -1.0]), "b": {"c": jnp.array(0.1)}}
+     >>> scale = {"a": jnp.array([[1.0, 2.0], [3.0, 4.0]]), "b": {"c": jnp.array(5.0)}}
+     >>> get_prior(loc, scale)
+     {'a': (Array([ 1., -1.], dtype=float64),
+     Array([[1., 2.],
+            [3., 4.]], dtype=float64)),
+    'b': {'c': (Array(0.1, dtype=float64, weak_type=True),
+      Array(5., dtype=float64, weak_type=True))}}
+
+    """  # noqa: E501
+    if not jax.tree.structure(loc) == jax.tree.structure(scale):
+        msg = "Prior mean and prior sd must have the same tree structure"
+        raise ValueError(msg)
+    return jax.tree.transpose(
+        outer_treedef=jax.tree.structure(("*", "*")),
+        inner_treedef=jax.tree.structure(loc),
+        pytree_to_transpose=(loc, scale),
+    )
+
+
+def match_prior_leaf(node):
+    """Match a Pytree node representing a prior, i.e. a 2-tuple of jax arrays.
+
+    This makes it possible to use jax.tree.map with a function that takes
+    a prior leaf as an argument.
+
+    Params:
+    -------
+    node: Any
+        A Pytree node.
+
+    Returns:
+    --------
+    bool
+        True if the node is a 2-tuple of jax arrays
+    """
+    return (
+        (isinstance(node, tuple))
+        and (isinstance(node[0], jax.Array))
+        and (len(node) == 2)
+    )
+
+
+def enzax_prior_logdensity(parameters: PyTree, prior: PyTree) -> Scalar:
+    """Get the prior log density for the parameters given the prior.
+
+    The distribution used depends on the shape of each prior location and scale
+    parameter: if they are the same shape, a normal distribution is used; if
+    the location is one dimension lower than the scale, a multivariate normal
+    distribution is used.
+
+    Params:
+    -------
+    parameters: PyTree
+        A PyTree representing the parameters.
+    prior: PyTree
+        A PyTree representing the prior. Each leaf is a tuple of the prior
+        location and the prior scale (i.e. the argument sd repeated as
+        appropriate
 
     """
 
-    def merge_leaves(
-        free_leaf: Float[Array, " _"] | None,
-        fixed_leaf: Float[Array, " _"] | None,
-    ) -> Float[Array, " _"]:
-        if free_leaf is None and fixed_leaf is None:
-            msg = "fixed_leaf and free_leaf cannot both be None"
-            raise ValueError(msg)
-        elif free_leaf is None:
-            return fixed_leaf  # pyright: ignore[reportReturnType]
-        elif fixed_leaf is None:
-            return free_leaf
+    def prior_logdensity_leaf(param_leaf, prior_leaf):
+        loc, scale = prior_leaf
+        if loc.shape == scale.shape:
+            return norm.logpdf(param_leaf, loc, scale).sum()
+        elif len(loc.shape) == len(scale.shape) - 1:
+            return multivariate_normal.logpdf(param_leaf, loc, scale).sum()
         else:
-            return fixed_leaf.at[jnp.isnan(fixed_leaf)].set(free_leaf)
-
-    return jax.tree.map(merge_leaves, free, fixed, is_leaf=lambda x: x is None)
-
-
-def enzax_prior_logdensity(prior: PyTree, parameters: PyTree) -> Scalar:
-    def enzax_prior_logdensity_leaf(param_prior, param_leaf):
-        loc, scale = param_prior
-        return norm.logpdf(param_leaf, loc, scale).sum()
+            msg = "Prior loc and scale have incompatible shape"
+            raise ValueError(msg)
 
     prior_logdensities = jax.tree.map(
-        enzax_prior_logdensity_leaf,
-        prior,
+        prior_logdensity_leaf,
         parameters,
-        is_leaf=lambda node: isinstance(node, tuple),
+        prior,
+        is_leaf=match_prior_leaf,
     )
     return jax.tree.reduce(operator.add, prior_logdensities)
 
 
-def enzax_log_likelihood(
-    observations: list[ObservationSet], conc, flux, enzyme
-) -> Scalar:
-    """Stub!"""
-    return jnp.array(1.0)
+def enzax_log_likelihood(conc, enzyme, flux) -> Scalar:
+    conc_hat, conc_obs, conc_err = conc
+    enz_hat, enz_obs, enz_err = enzyme
+    flux_hat, flux_obs, flux_err = flux
+    llik_conc = norm.logpdf(
+        jnp.log(conc_obs), jnp.log(conc_hat), conc_err
+    ).sum()
+    llik_enz = norm.logpdf(jnp.log(enz_obs), jnp.log(enz_hat), enz_err).sum()
+    llik_flux = norm.logpdf(flux_obs, loc=flux_hat, scale=flux_err).sum()
+    return llik_conc + llik_enz + llik_flux
 
 
 def enzax_log_density(
     free_parameters: PyTree,
     structure: RateEquationKineticModelStructure,
-    observations: list[ObservationSet],
+    measurements: PyTree,
     prior: PyTree,
     fixed_parameters: PyTree | None = None,
     guess: Float[Array, " _"] | None = None,
@@ -166,144 +181,17 @@ def enzax_log_density(
         parameters = eqx.combine(free_parameters, fixed_parameters)
     else:
         parameters = free_parameters
-    # find the steady state concentration and flux
     model = RateEquationModel(parameters, structure)
     steady = get_kinetic_model_steady_state(model, guess)
-    conc = get_conc(steady, parameters["log_conc_unbalanced"], structure)
-    flux = model.flux(steady)
+    conc_hat = get_conc(steady, parameters["log_conc_unbalanced"], structure)
     flat_log_enzyme, _ = ravel_pytree(parameters["log_enzyme"])
-    enzyme = jnp.exp(flat_log_enzyme)
-    log_prior = enzax_prior_logdensity(prior, free_parameters)
-    log_likelihood = enzax_log_likelihood(observations, conc, flux, enzyme)
-    # likelihood
+    enz_hat = jnp.exp(jnp.array(flat_log_enzyme))
+    flux_hat = model.flux(steady)
+    conc_msts, enz_msts, flux_msts = measurements
+    log_prior = enzax_prior_logdensity(free_parameters, prior)
+    log_likelihood = enzax_log_likelihood(
+        (conc_hat, *conc_msts),
+        (enz_hat, *enz_msts),
+        (flux_hat, *flux_msts),
+    )
     return log_prior + log_likelihood
-
-
-def search(to_search: dict, queries: tuple[str, ...]):
-    out = to_search
-    for q in queries:
-        out = out[q]
-    return out
-
-
-def is_none(x):
-    return x is None
-
-
-def split_given_free(
-    params: PyTree,
-    free_spec: list[FreeParamSpec],
-) -> tuple[PyTree, PyTree]:
-    """Split parameters into free and fixed given free_spec.
-
-
-    Parameters
-    ----------
-    params_all : PyTree
-        The parameters to split.
-    free_spec : list[FreeParamSpec]
-        A specification of the free parameters. Mutually exclusive with
-        fixed_spec. All parameters in params_all except those specified in
-         free_spec are considered fixed.
-
-    Returns
-    -------
-    free : PyTree
-        PyTree of free parameters.
-    fixed : PyTree
-        PyTree of fixed parameters.
-
-    Examples
-    --------
-    ```python
-    from enzax.examples.linear import parameters
-    from enzax.statistical_modelling import FreeParamSpec, split_given_free
-    import jax.numpy as jnp
-
-    free_spec = [
-        FreeParamSpec(path=("log_kcat", "r1"), ix=(), inits=jnp.array(0.3)),
-        FreeParamSpec(path=("temperature",), ix=(), inits=jnp.array(3.3)),
-        FreeParamSpec(path=("dgf",), ix=(0,), inits=jnp.array(33.3)),
-    ]
-    free_params, fixed_params = split_given_free(parameters, free_spec)
-    print(free_params)
-    print(fixed_params)
-
-    ```
-
-    """
-    fixed = deepcopy(params)
-    free = jax.tree.map(lambda _: None, params)
-    for ps in free_spec:
-        filter_spec = partial(search, queries=ps.path)
-        free = eqx.tree_at(filter_spec, free, replace=ps.inits, is_leaf=is_none)
-        fixed = eqx.tree_at(
-            filter_spec,
-            fixed,
-            replace_fn=lambda arr: arr.at[ps.ix].set(jnp.nan),
-            is_leaf=is_none,
-        )
-    return free, fixed
-
-
-def split_given_fixed(
-    params: PyTree,
-    fixed_spec: list[FixedParamSpec],
-) -> tuple[PyTree, PyTree]:
-    """Split parameters into free and fixed given fixed_spec.
-
-    params : PyTree
-        The parameters to split.
-
-    fixed_spec : list[FixedParamSpec]
-        A specification of the fixed parameters. Mutually exclusive with
-        free_spec. All parameters in params_all except those specified in
-        fixed_spec are considered free.
-
-    Returns
-    -------
-    free : PyTree
-        PyTree of free parameters.
-    fixed : PyTree
-        PyTree of fixed parameters.
-
-    Examples
-    --------
-    ```python
-
-    from enzax.examples.linear import parameters
-    from enzax.statistical_modelling import FixedParamSpec, split_given_fixed
-    import jax.numpy as jnp
-
-    fixed_spec = [
-        FixedParamSpec(
-            path=("log_kcat", "r1"),
-            ix=(),
-            fixed_values=jnp.array(0.3),
-        ),
-        FixedParamSpec(
-            path=("temperature",),
-            ix=(),
-            fixed_values=jnp.array(3.3),
-        ),
-        FixedParamSpec(path=("dgf",), ix=(0,), fixed_values=jnp.array(33.3)),
-    ]
-    free_params, fixed_params = split_given_fixed(parameters, fixed_spec)
-    print(free_params)
-    print(fixed_params)
-    """
-
-    fixed = jax.tree.map(lambda _: None, params)
-    free = deepcopy(params)
-    for ps in fixed_spec:
-        filter = partial(search, queries=ps.path)
-        arr = filter(params)
-        fixed_p = jnp.full_like(arr, jnp.nan).at[ps.ix].set(ps.fixed_values)  # pyright: ignore[reportArgumentType]
-        if len(arr.shape) > 0:  # pyright: ignore[reportAttributeAccessIssue]
-            mask = jnp.ones_like(arr, dtype=jnp.int16).at[ps.ix].set(0)  # pyright: ignore[reportArgumentType]
-            free_p = arr[mask == 1]
-        else:
-            free_p = None
-        fixed = eqx.tree_at(filter, fixed, replace=fixed_p, is_leaf=is_none)
-        free = eqx.tree_at(filter, free, replace=free_p, is_leaf=is_none)
-    return free, fixed
