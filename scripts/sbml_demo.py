66d170bfc785b459db4c2268253e17c8a9806f18
Teddy Groves <groves.teddy@gmail.com>
2025-03-24 11:06:15 +0100

    Update demo

--- scripts/sbml_demo.py
+++ scripts/sbml_demo.py
@@ -4,19 +4,29 @@ import jax
 import jax.numpy as jnp
 
 from enzax.sbml import load_libsbml_model_from_file, sbml_to_enzax
-from enzax.steady_state import get_kinetic_model_steady_state
+from enzax.steady_state import get_steady_state
 
 jax.config.update("jax_enable_x64", True)
 
-file_path = Path("tests") / "data" / "exampleode.xml"
-model_libsbml = load_libsbml_model_from_file(file_path)
-model = sbml_to_enzax(model_libsbml)
+SBML_FILE_PATH = Path("tests") / "data" / "exampleode.xml"
 
-y0 = jnp.array([2.0, 4])
-print(f"Flux at {str(y0)}: " + str(model.flux(y0)))
 
-print(f"dcdt at {str(y0)}, t=1: " + str(model.dcdt(t=1, conc=y0)))
+def main():
+    model_libsbml = load_libsbml_model_from_file(SBML_FILE_PATH)
+    model, parameters = sbml_to_enzax(model_libsbml)
 
-guess = jnp.full((2), 0.01)
-steady_state = get_kinetic_model_steady_state(model, guess)
-print("Steady state: " + str(steady_state))
+    y0 = jnp.array([2.0, 4])
+    print(f"Flux at {str(y0)}: " + str(model.flux(y0, parameters)))
+
+    print(
+        f"dcdt at {str(y0)}, t=1: "
+        + str(model.dcdt(conc=y0, parameters=parameters))
+    )
+
+    guess = jnp.full((2), 0.01)
+    steady_state = get_steady_state(model, guess, parameters)
+    print("Steady state: " + str(steady_state))
+
+
+if __name__ == "__main__":
+    main()
