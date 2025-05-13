fe86df62f2c795c7f4445a4fcd39def395f611cc
AlberteSloth <afjsl@dtu.dk>
2025-05-13 12:19:03 +0200

    Smallbone model18 added

--- scripts/sbml_demo.py
+++ scripts/sbml_demo.py
@@ -1,21 +1,71 @@
 from pathlib import Path
 
+import functools
+import logging
+import warnings
+
 import jax
 import jax.numpy as jnp
+from jaxtyping import PyTree
 
-from enzax.sbml import load_libsbml_model_from_file, sbml_to_enzax
+import equinox as eqx
+
+from enzax.examples.smallbone import load_smallbone, get_conc_assingment_species, enzax_log_density_sbml
 from enzax.steady_state import get_steady_state
+from enzax.statistical_modelling import prior_from_truth
+from enzax.mcmc import run_nuts
+
+SEED = 1234
 
 jax.config.update("jax_enable_x64", True)
 
-SBML_FILE_PATH = Path("tests") / "data" / "exampleode.xml"
+#SBML_FILE_PATH = Path("enzax") / "examples" / "smallbone_model18.xml"
 
+def get_free_params(params: PyTree) -> PyTree:
+    return(
+        params['kcat_ADH_ADH1'],
+        params['Vmax_ATPase'],
+        params['kcat_ENO_ENO1'],
+        params['kcat_ENO_ENO2'], 
+        params['kcat_FBA'],
+        params['Vmax_GPD'],
+        params['kcat_GPM'],
+        params['Vmax_GPP'],
+        params['kcat_HXK_HXK1'], 
+        params['kcat_HXK_HXK2'], 
+        params['kcat_PDC_PDC1'],
+        params['kcat_PDC_PDC5'],
+        params['kcat_PDC_PDC6'],
+        params['kcat_PFK'], 
+        params['kcat_PGI'],
+        params['kcat_PGK'],
+        params['Vmax_PGM'],
+        params['kcat_PYK_CDC19'],
+        params['kcat_TDH_TDH1'],
+        params['kcat_TDH_TDH2'],
+        params['kcat_TDH_TDH3'],
+        params['kcat_TPI'],
+        params['Vmax_TPP'],
+        params['Vmax_TPS'],
+        params['Vmax_UGP'],
+        params['Vmax_HXT'],
+    )
+
+def simulate(key, truth, error):
+    key_conc, key_flux = jax.random.split(key, num=2)
+    true_conc, true_flux = truth
+    conc_err, flux_err = error
+    return (
+        jnp.exp(jnp.log(true_conc) + jax.random.normal(key_conc) * conc_err),
+        true_flux + jax.random.normal(key_flux) * flux_err,
+    )
 
 def main():
-    model_libsbml = load_libsbml_model_from_file(SBML_FILE_PATH)
-    model, parameters = sbml_to_enzax(model_libsbml)
+    # The path should be updated.
+    file_path = "C:/Users/afjsl/Documents/enzax_clone/src/enzax/examples/smallbone2013_model18_modified.xml"
+    model, parameters, initial_conc = load_smallbone(file_path)
 
-    y0 = jnp.array([2.0, 4])
+    y0 = initial_conc
     print(f"Flux at {str(y0)}: " + str(model.flux(y0, parameters)))
 
     print(
@@ -23,10 +73,71 @@ def main():
         + str(model.dcdt(conc=y0, parameters=parameters))
     )
 
-    guess = jnp.full((2), 0.01)
+    guess = initial_conc
     steady_state = get_steady_state(model, guess, parameters)
     print("Steady state: " + str(steady_state))
 
+    parameters_log = jax.tree.map(lambda x: jnp.log(x), parameters)
+
+    is_free = eqx.tree_at(
+        get_free_params,
+        jax.tree.map(lambda _: False, parameters),
+        replace_fn=lambda _: True,
+    )
+
+    _, fixed_params = eqx.partition(parameters, is_free)
+    free_params_log, _ = eqx.partition(parameters_log, is_free)
+    prior_log = prior_from_truth(free_params_log, sd=0.1)
+
+    true_conc = get_conc_assingment_species(
+        steady_state,
+        parameters,
+        model
+    )
+
+    true_flux = model.flux(steady_state, parameters)
+
+    # simulate observations
+    conc_err = jnp.full_like(true_conc, 0.03) 
+    flux_err = jnp.full_like(true_flux, 0.05) 
+    key = jax.random.key(SEED)
+    key_sim, key_nuts = jax.random.split(key, num=2)
+    measurement_errors = (conc_err, flux_err)
+    measurement_values = simulate(
+        key=key_sim,
+        truth=(true_conc, true_flux),
+        error=measurement_errors,
+    )
+    measurements = tuple(zip(measurement_values, measurement_errors))
+
+    posterior_log_density = functools.partial(
+        enzax_log_density_sbml,
+        model=model,
+        fixed_parameters=fixed_params,
+        measurements=measurements,
+        prior_log=prior_log,
+        guess=guess,
+    )
+
+    states, info = run_nuts(
+        logdensity_fn=posterior_log_density,
+        rng_key=key_nuts,
+        init_parameters=free_params_log,
+        num_warmup=10,
+        num_samples=10,
+        initial_step_size=0.0001,
+        max_num_doublings=8,
+        is_mass_matrix_diagonal=False,
+        target_acceptance_rate=0.95,
+    )
+
+    if jnp.any(info.is_divergent):
+        n_divergent = info.is_divergent.sum()
+        msg = f"There were {n_divergent} post-warmup divergent transitions."
+        warnings.warn(msg)
+    else:
+        logging.info("No post-warmup divergent transitions!")
+        print("No post-warmup divergent transitions!")
 
 if __name__ == "__main__":
     main()
---
+++ src/enzax/examples/smallbone.py
@@ -0,0 +1,143 @@
+import libsbml
+import re
+import equinox as eqx
+import sympy2jax
+
+import jax
+from jax import numpy as jnp
+from jax.scipy.stats import norm
+from jaxtyping import Array, Float, PyTree, Scalar
+
+from enzax.kinetic_model import KineticModelSbml
+from enzax.sbml import load_libsbml_model_from_file, sbml_to_enzax
+from enzax.steady_state import get_steady_state
+from enzax.statistical_modelling import enzax_prior_logdensity
+
+jax.config.update("jax_enable_x64", True)
+
+def load_smallbone(path):
+    """
+    Function for parsing the Smallbone model. Incures all parameters have specific names. Checks for initial concentrations set at 0 and changes them to 1e-5.
+
+    Parameters 
+    ----------
+    path: str
+        The path to SBML-files
+    
+    Returns
+    --------
+    model:
+        A kinetic model 
+    parameters: PyTree
+        Parameters defined in the SBML-file
+    init_conc: a JAX array of floats
+        The initial concentration defined in the SBML-file
+    """
+
+    file_path = path
+
+    model_libsbml01 = load_libsbml_model_from_file(file_path)
+
+    model_libsbml = model_libsbml01.clone()
+
+    [ic.setInitialConcentration(1e-5) for ic in model_libsbml.getListOfSpecies() if ic.getInitialConcentration() == 0]
+
+    for r in model_libsbml.getListOfReactions():
+        oldnames = [p.getName() for p in r.getKineticLaw().getListOfParameters()]
+        for p in r.getKineticLaw().getListOfParameters():
+            p.setId(p.getId()+'_'+r.getId())
+            p.setName(p.getName()+'_'+r.getId())
+        newnames = [p.getName() for p in r.getKineticLaw().getListOfParameters()]
+        formula_string = r.getKineticLaw().getFormula()
+        for o, n in zip(oldnames, newnames):
+            pattern = rf'\b{o}\b'
+            formula_string = re.sub(pattern, n, formula_string)
+        r.getKineticLaw().setMath(libsbml.parseL3Formula(formula_string))
+        
+    model, parameters = sbml_to_enzax(model_libsbml)
+
+    init_conc =jnp.array([
+        b.getInitialConcentration()
+        for b in model_libsbml.getListOfSpecies()
+        if not b.boundary_condition
+    ]) 
+    return model, parameters, init_conc
+
+
+@eqx.filter_jit()
+def get_conc_assingment_species(balanced, parameters, model):
+    """ 
+    Function for combining concentration when some unbalanced species are defined as assignments. 
+
+    Parameters:
+    -----------
+    balanced: JAX array of type float
+        A JAX array containing the concentration for balaned species
+    parameters: PyTree
+        A dictonary containing the parameters, including unbalanced species. 
+    model: KineticModelSbml
+
+    """
+    parameters_new = parameters.copy()
+    conc = jnp.zeros(model.S.shape[0])
+    for a in model.sym_module[1].keys():
+        parameters_new.update(
+            {
+                a: sympy2jax.SymbolicModule(model.sym_module[1][a])(
+                    **parameters_new,
+                    **dict(zip(model.balanced_species, balanced)),
+                )
+            }
+        )
+    conc = conc.at[model.balanced_species_ix].set(balanced)
+    conc = conc.at[model.unbalanced_species_ix].set(jnp.array([
+        parameters_new[a] 
+        for a in model.unbalanced_species
+    ]))
+    return conc
+
+
+@jax.jit
+def enzax_log_likelihood(conc, flux) -> Scalar:
+    conc_hat, conc_obs, conc_err = conc
+    flux_hat, flux_obs, flux_err = flux
+    llik_conc = norm.logpdf(
+        jnp.log(conc_obs), jnp.log(conc_hat), conc_err
+    ).sum()
+    llik_flux = norm.logpdf(flux_obs, loc=flux_hat, scale=flux_err).sum()
+    return llik_conc + llik_flux
+
+
+@jax.jit
+def enzax_log_density_sbml(
+    free_parameters_log: PyTree,
+    model: KineticModelSbml,
+    measurements: PyTree,
+    prior_log: PyTree,
+    fixed_parameters: PyTree | None = None,
+    guess: Float[Array, " _"] | None = None,
+) -> Scalar:
+    
+    free_parameters = jax.tree.map(lambda x: jnp.exp(x), free_parameters_log)
+
+    if guess is None:
+        guess = jnp.full((len(model.balanced_species_ix)), 0.01)
+    if fixed_parameters is not None:
+        parameters = eqx.combine(free_parameters, fixed_parameters)
+    else:
+        parameters = free_parameters
+
+    steady = get_steady_state(model, guess, parameters)
+    conc_hat = get_conc_assingment_species(steady, parameters, model)
+    flux_hat = model.flux(steady, parameters)
+    conc_msts, flux_msts = measurements
+    log_prior = enzax_prior_logdensity(free_parameters_log, prior_log)
+    log_likelihood = enzax_log_likelihood(
+        (conc_hat, *conc_msts),
+        (flux_hat, *flux_msts),
+    )
+    log_posterior = log_prior + log_likelihood
+    if steady is None:
+        log_posterior = -jnp.inf
+
+    return log_posterior
